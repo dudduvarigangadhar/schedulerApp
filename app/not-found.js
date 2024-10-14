edc941482baebced4fd26847081f813916e81303
gangadhar <gangadhardudduvari@gmail.com>
2024-10-14 16:33:32 +0530

    created routings

---
+++ app/not-found.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function NotFound() {
+  return (
+    <div className='text-4xl font-extrabold w-screen pt-96 grid place-items-center'>
+      <h1>404 - Page Not Found</h1>
+    </div>
+  )
+}
